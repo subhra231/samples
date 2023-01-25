da4aa83832fa9f1d26041d8f146c2805a0b1528f
subhra231 <subhra.nits@gmail.com>
2023-01-25 11:33:56 -0800

    Initial Commit

---
+++ framework/wcf/Extensibility/Transport/AzureQueueStorageClient/AzureQueueStorageClient/SampleProfileUdpBindingCollectionElement.cs
@@ -0,0 +1,14 @@
+﻿//----------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//----------------------------------------------------------------
+
+namespace Microsoft.Samples.Udp
+{
+
+    /// <summary>
+    /// Binding Section for Udp. Implements configuration for SampleProfileUdpBinding.
+    /// </summary>
+   /* public class SampleProfileUdpBindingCollectionElement : StandardBindingCollectionElement<SampleProfileUdpBinding, SampleProfileUdpBindingConfigurationElement>
+    {
+    }*/
+}
